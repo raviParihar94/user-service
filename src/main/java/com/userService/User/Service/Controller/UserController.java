fb7fb8dc41e10acdb624995f1e865e72d88385e9
raviParihar94 <pariharravi480@gmail.com>
2023-10-17 23:01:28 +0530

    Creating User and Its services

---
+++ src/main/java/com/userService/User/Service/Controller/UserController.java
@@ -0,0 +1,46 @@
+package com.userService.User.Service.Controller;
+
+import com.userService.User.Service.Entity.User;
+import com.userService.User.Service.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    // create User
+    @PostMapping("/saveUser")
+    public ResponseEntity<User> createUser(@RequestBody  User user){
+        User user1 = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+
+    }
+
+    // get User by ID
+    @GetMapping("/{userID}")
+
+    public  ResponseEntity<User> getSingleUserByID( @PathVariable  Integer userId){
+        User userByID = userService.getUserByID(userId);
+        return ResponseEntity.ok(userByID);
+
+    }
+
+    // all users
+    public  ResponseEntity<List<User>> getAllUsers(){
+        List<User> allUser = userService.getAllUser();
+        return ResponseEntity.ok(allUser);
+    }
+
+    // update user
+
+
+    // delete user
+}
